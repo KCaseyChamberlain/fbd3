5c5952980040a010659a1884a4de6ea5b8fea84c
kcaseychamberlain <caseygchamberlain@gmail.com>
2023-08-04 07:06:37 -0600

    graph finished and functional

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const name = document.querySelector('#name');
 const cost = document.querySelector('#cost');
 const error = document.querySelector('#error');
 
-
 form.addEventListener('submit', (e) => {
+
     e.preventDefault();
 
     if (name.value && cost.value) {
@@ -15,13 +15,13 @@ form.addEventListener('submit', (e) => {
         };
 
         db.collection('expenses').add(item).then(res => {
-            error.textContent = "";
-            name.value = "";
-            cost.value = "";
-        })
+            error.textContent = '';
+            name.value = '';
+            cost.value = '';
+        });
 
     } else {
-        error.textContent = 'Please enter values before submitting'
+        error.textContent = 'Please enter values before submitting';
     }
 
-})
+});
